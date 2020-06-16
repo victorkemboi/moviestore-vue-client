7448dd96933e5c2dff1c60831490d985c400fbb3
Victor Kiprop <kemboivictor@outlook.com>
2020-06-16 18:49:29 +0300

    starting home page and navbar

---
+++ src/components/SideNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Movie Store</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideNav",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #ffffff;
+}
+</style>
